4ff97c03ce33d7e05ab160b871fe65610061ea89
RAMON ARAUJO OLIVEIRA <ramonaololiveira@gmail.com>
2025-06-09 01:18:13 -0300

    Vesion: 001.000.000.000

---
+++ src/Metadata.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Laravel\Folio;
+
+use Illuminate\Support\Collection;
+
+class Metadata
+{
+    /**
+     * The current global instance of the metadata, if any.
+     */
+    protected static ?self $instance = null;
+
+    /**
+     * Create a new metadata instance.
+     */
+    protected function __construct(
+        public ?string $name = null,
+        public Collection $middleware = new Collection,
+        public bool $withTrashed = false
+    ) {
+        //
+    }
+
+    /**
+     * Get the current metadata instance or create a new one.
+     */
+    public static function instance(): static
+    {
+        return static::$instance ??= new static;
+    }
+
+    /**
+     * Flush the current global instance of the metadata.
+     */
+    public static function flush(): void
+    {
+        static::$instance = null;
+    }
+}
